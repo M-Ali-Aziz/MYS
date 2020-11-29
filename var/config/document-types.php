7272926187e380375d8df05731c6ed1995708ffe
Mohammed Ali <mohammed_ali.aziz_assi@ldc.lu.se>
2020-11-29 19:32:22 +0100

    Added config admin UI

--- var/config/document-types.php
+++ var/config/document-types.php
@@ -1,5 +1,17 @@
 <?php 
 
 return [
-
+    1 => [
+        "id" => 1,
+        "name" => "Landing_page",
+        "group" => "",
+        "module" => "AppBundle",
+        "controller" => "@AppBundle\\Controller\\ContentController",
+        "action" => "start",
+        "template" => NULL,
+        "type" => "page",
+        "priority" => 0,
+        "creationDate" => 1606483223,
+        "modificationDate" => 1606489143
+    ]
 ];
---
+++ var/config/predefined-properties.php
@@ -0,0 +1,30 @@
+<?php 
+
+return [
+    1 => [
+        "id" => 1,
+        "name" => "Navigation root",
+        "description" => NULL,
+        "key" => "navigation_root",
+        "type" => "document",
+        "data" => "",
+        "config" => NULL,
+        "ctype" => "document",
+        "inheritable" => TRUE,
+        "creationDate" => 1606490241,
+        "modificationDate" => 1606490271
+    ],
+    2 => [
+        "id" => 2,
+        "name" => "Footer",
+        "description" => NULL,
+        "key" => "footer",
+        "type" => "document",
+        "data" => NULL,
+        "config" => NULL,
+        "ctype" => "document",
+        "inheritable" => TRUE,
+        "creationDate" => 1606490413,
+        "modificationDate" => 1606490427
+    ]
+];
